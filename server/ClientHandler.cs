4982cd4423fb66a40293d0b3ff57a3930d630452
Maks23-21-11 <piletskaya2004@icloud.com>
2024-04-10 10:30:57 +0300

    123

---
+++ server/ClientHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    public class ClientHandler
+    {
+        public TcpClient clientSocket;
+        public void RunClient()
+        {
+            StreamReader readerStream = new StreamReader(clientSocket.GetStream());
+            NetworkStream writeStream = clientSocket.GetStream();
+
+            string returnData = readerStream.ReadLine();
+            string name = returnData;
+
+            Console.WriteLine($"Welcome {name} to the server");
+
+            while (true)
+            {
+                returnData = readerStream.ReadLine();
+                if (returnData.IndexOf("QUIT") > -1)
+                {
+                    Console.WriteLine($"Goodbay {name}");
+                    break;
+                }
+
+                Console.WriteLine($"{name} : {returnData}");
+                returnData += "\r\n";
+                byte[] dataWrite = Encoding.ASCII.GetBytes(returnData);
+
+                writeStream.Write(dataWrite, 0, dataWrite.Length);
+            }
+            clientSocket.Close();
+        }
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace server
+{
+    internal class Program
+    {
+        const int ECHO_PORT = 8080;
+        public static int nClient = 0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetLocalIPAddress());
+            try 
+            {
+                TcpListener clientListener = new TcpListener(ECHO_PORT);
+                clientListener.Start();
+
+                Console.WriteLine("Waiting for connections...");
+
+                while(nClient<5)
+                {
+                    TcpClient client = clientListener.AcceptTcpClient();
+                    ClientHandler cHandler = new ClientHandler();
+                    cHandler.clientSocket = client;
+
+                    Thread clientThread = new Thread(new ThreadStart(cHandler.RunClient));
+                    clientThread.Start();
+                    nClient++;
+                }
+                clientListener.Stop();
+            }
+            catch(Exception e) 
+            { 
+                Console.WriteLine(e.ToString()); 
+            }
+            
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach(var ip in host.AddressList)
+            {
+                if(ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+
+            throw new Exception("no Network adapters with an IPv4 address in the system");
+        }
+    }
+}
